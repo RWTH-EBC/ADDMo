68c4147795e87ae1da26e21846ef5ee5e050219b
mithilkm8797 <mithil.kashipurad@rwth-aachen.de>
2021-08-08 19:13:20 +0200

    Removed and fixed all SV references from PeriodSelection.py

--- 02_Tool/PeriodSelection.py
+++ 02_Tool/PeriodSelection.py
@@ -3,10 +3,7 @@ import time
 import pandas as pd
 from openpyxl import load_workbook
 from Functions.PlotFcn import plot_TimeSeries
-import sys
 import os
-import SharedVariables as SV
-
 
 # -------------------------------------------------------------------------------
 def manual_period_select(Data, StartDate, EndDate):
@@ -15,9 +12,9 @@ def manual_period_select(Data, StartDate, EndDate):
     return Data
 
 
-def timeseries_plotting(Data, Scaled):
+def timeseries_plotting(DT_Setup_object, Data, Scaled):
     # set folder for sensor and resolution
-    CorrelationResultsFolder = "%s/%s" % (SV.ResultsFolder, "TimeSeries_plotting")
+    CorrelationResultsFolder = "%s/%s" % (DT_Setup_object.ResultsFolder, "TimeSeries_plotting")
     # check if directory exists, if not, create it
     if not os.path.exists(CorrelationResultsFolder):
         os.makedirs(CorrelationResultsFolder)
@@ -32,16 +29,16 @@ def timeseries_plotting(Data, Scaled):
 def main(DT_Setup_object, DT_RR_object):
     print("PeriodSelection")
     startTime = time.time()
-    Data = pd.read_pickle(os.path.join(SV.PathToPickles, "ThePickle_from_Preprocessing" + '.pickle'))
+    Data = pd.read_pickle(os.path.join(DT_Setup_object.PathToPickles, "ThePickle_from_Preprocessing" + '.pickle'))
 
     if DT_Setup_object.TimeSeriesPlot == True:
-        if os.path.isfile(os.path.join(SV.ResultsFolder,
+        if os.path.isfile(os.path.join(DT_Setup_object.ResultsFolder,
                                        "ScalerTracker.save")):  # check if a scaler is used, if a scaler is used the file "ScalerTracker" was created
             timeseries_plotting(Data, True)  # plot scaled data
-            timeseries_plotting(pd.read_pickle(os.path.join(SV.PathToPickles, "ThePickle_from_ImportData" + '.pickle')),
+            timeseries_plotting(pd.read_pickle(os.path.join(DT_Setup_object.PathToPickles, "ThePickle_from_ImportData" + '.pickle')),
                                 False)  # plot raw data
         else:
-            timeseries_plotting(Data, False)
+            timeseries_plotting(DT_Setup_object, Data, False)
     if DT_Setup_object.ManSelect == True:
         Data = manual_period_select(Data, DT_Setup_object.StartDate, DT_Setup_object.EndDate)
 
@@ -49,11 +46,11 @@ def main(DT_Setup_object, DT_RR_object):
     DT_RR_object.period_selection_time = endTime - startTime
     DT_RR_object.df_period_selection_data = Data
     # save dataframe to pickle
-    Data.to_pickle(os.path.join(SV.PathToPickles, "ThePickle_from_PeriodSelection" + '.pickle'))
+    Data.to_pickle(os.path.join(DT_Setup_object.PathToPickles, "ThePickle_from_PeriodSelection" + '.pickle'))
 
     # save dataframe in the ProcessedInputData excel file
 
-    ExcelFile = os.path.join(SV.ResultsFolder, "ProcessedInputData_%s.xlsx" % (DT_Setup_object.NameOfExperiment))
+    ExcelFile = os.path.join(DT_Setup_object.ResultsFolder, "ProcessedInputData_%s.xlsx" % (DT_Setup_object.NameOfExperiment))
     book = load_workbook(ExcelFile)
     writer = pd.ExcelWriter(ExcelFile, engine="openpyxl")
     writer.book = book

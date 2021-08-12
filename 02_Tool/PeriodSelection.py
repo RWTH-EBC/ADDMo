d0805af498494acecd67b8a2093218a22ba7ce20
Martin <martin.raetz@rwth-aachen.de>
2021-08-12 13:44:17 +0200

    reference dtrr instead of dts

--- 02_Tool/PeriodSelection.py
+++ 02_Tool/PeriodSelection.py
@@ -31,8 +31,8 @@ def main(DT_Setup_object, DT_RR_object):
     print("PeriodSelection")
     startTime = time.time()
 
-    dataScaled = DT_Setup_object.df_preprocessing_data
-    dataRaw = DT_Setup_object.df_import_data
+    dataScaled = DT_RR_object.df_preprocessing_data
+    dataRaw = DT_RR_object.df_import_data
 
     if DT_Setup_object.TimeSeriesPlot == True:
         if os.path.isfile(os.path.join(DT_Setup_object.ResultsFolder,"ScalerTracker.save")):  # check if a scaler is used, if a scaler is used the file "ScalerTracker" was created

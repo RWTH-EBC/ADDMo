adac649584693d5ba619125e50a403a6ccea2f68
Pohlmann <thi-dpo@eonerc.rwth-aachen.de>
2022-01-26 16:48:53 +0100

    AIC&BIC

--- 02_Tool/Functions/ErrorMetrics.py
+++ 02_Tool/Functions/ErrorMetrics.py
@@ -13,6 +13,9 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from statsmodels.tools.eval_measures import rmse
+from math import log
+from math import exp
+from math import factorial
         
 def mean_absolute_scaled_error(measuredData, predictData, mae):
     # Calculate the mean absolute scaled error, with IN-SAMPLE naive method
@@ -37,7 +40,9 @@ def standarddeviation(measuredData, predictData):
     error = measuredData - predictData
     STD = statistics.pstdev(error)
     return STD
-
+def residualsumsquares(measuredData,predictData):  #wert fast gleich wie mse
+    rss =1/(len(measuredData)) *sum((measuredData-predictData)**2)
+    return rss
 def evaluation(measuredData, predictData):
     #Evaluation
     R2 = r2_score(measuredData, predictData)
@@ -51,7 +56,75 @@ def evaluation(measuredData, predictData):
     STD = standarddeviation(measuredData, predictData)
     return (R2, STD, RMSE, MAPE, MAE)
 
+def calculate_AIC(n,num_params,measuredData,predictData):
+
+    mse = mean_squared_error(measuredData, predictData)
+
+    if (n/num_params) < 40:
+        aic = calculate_AICc(n,mse,num_params)
+    else:
+        aic = n*log(mse) + 2*num_params
+    return aic
+    """
+    else:
+        listaic=[]
+        for i in range(len(num_params)):
+            if (n / num_params[i]) < 40:
+                aic = calculate_AICc(n, mse, num_params[i])
+            else:
+                aic = n * log(mse) + 2 * num_params[i]
+            listaic.append(aic)
+        return listaic
+    """
+
+def calculate_BIC(n,num_params, measuredData, predictData):
+    mse = mean_squared_error(measuredData, predictData)
+    bic = n*log(mse)+ num_params *log(n)
+    return bic
+    """
+    else:
+        listbic=[]
+        for i in range(len(num_params)):
+            bic = n*log(mse)+ num_params[i] *log(n)
+            listbic.append(bic)
+        return listbic
+    """
 
 
+def calculate_AICc(n,mse,num_params): # korrigierte AIC
+    aicc = n*log(mse) + 2*num_params +(2*num_params*(num_params+1))/(n-num_params-1)
+    return aicc
+def mixed_kpi1(score,scoreweight,aic,aicweight):
+    kpiscore= scoreweight * exp(score) * aicweight * aic
+    return kpiscore
+def mixed_kpi2(aic,aicweight,bic,bicweight):
+    if aic < -15000:
+        kpiscore = 0.3 * aic + 0.7 * bic
+    else:
+        kpiscore = aicweight * aic + bicweight * bic
+    return kpiscore
+def mixed_kpi3(score,scoreweight,kpi,kpiweight):
+    kpiscore= scoreweight * exp(score) * kpiweight*kpi
+    return kpiscore
+
+def calc_AICSVR(n,epsilon, measuredData, predictData,features):     #scheint nicht gut zu funktionieren !!
+    mse = mean_squared_error(measuredData, predictData)
+    measuredData2 = measuredData.values
+    R = empiricalrisk(n,epsilon,measuredData2,predictData)
+
+    K = 0
+    for i in range(features):
+        temp = factorial(features)/(factorial(i)*factorial(features-i))
+        K = K +temp
+    aic =  R + 2*K*mse/(n-K)
+
+    return aic
 
 
+def empiricalrisk(n,epsilon,measuredData,predictData):
+    tempR = 0
+    for i in range(n):
+        temp =  max((abs(measuredData[i]-predictData[i])-epsilon),0 )
+        tempR = tempR+ temp
+    R = tempR/n
+    return R

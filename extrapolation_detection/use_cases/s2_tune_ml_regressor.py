92a58ef3aa66eb0a7127a925b874653d54953f77
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-03-28 14:47:40 +0100

    also

--- extrapolation_detection/use_cases/s2_tune_ml_regressor.py
+++ extrapolation_detection/use_cases/s2_tune_ml_regressor.py
@@ -14,8 +14,12 @@ from extrapolation_detection.use_cases.config.ed_experiment_config import (
 
 
 def exe_tune_regressor(config: ExtrapolationExperimentConfig):
-    xy_training = loading_saving.read_csv("xy_train", directory=config.experiment_folder)
-    xy_validation = loading_saving.read_csv("xy_val", directory=config.experiment_folder)
+    xy_training = loading_saving.read_csv(
+        "xy_train", directory=config.experiment_folder
+    )
+    xy_validation = loading_saving.read_csv(
+        "xy_val", directory=config.experiment_folder
+    )
 
     # training data of extrapolation experiment is used for model tuning
     xy_train_val = pd.concat([xy_training, xy_validation])
@@ -25,7 +29,12 @@ def exe_tune_regressor(config: ExtrapolationExperimentConfig):
 
     model_tuner = ModelTuner(config=config.config_model_tuning)
     model_dict = model_tuner.tune_all_models(x_train_val, y_train_val)
-    best_model = model_tuner.get_best_model(model_dict)
+    best_model_name = model_tuner.get_best_model_name(model_dict)
+    best_model = model_tuner.get_model(model_dict, best_model_name)
+    best_model_val_score = model_tuner.get_model_validation_score(
+        model_dict, best_model_name
+    )
+    best_model_params = best_model.get_params()
     regressor: AbstractMLModel = best_model
 
     # generate prediction for fit period
@@ -36,23 +45,19 @@ def exe_tune_regressor(config: ExtrapolationExperimentConfig):
     regressor_directory = os.path.join(config.experiment_folder, "regressors")
     loading_saving.write_pkl(regressor, "regressor", directory=regressor_directory)
 
-    loading_saving.write_csv(
-        xy_train_val, "xy_regressor_fit", directory=regressor_directory
-    )
-    loading_saving.write_csv(
-        x_train_val, "x_regressor_fit", directory=regressor_directory
-    )
-    loading_saving.write_csv(
-        y_train_val, "y_regressor_fit", directory=regressor_directory
-    )
-    loading_saving.write_csv(
-        y_pred, "pred_regressor_fit", directory=regressor_directory
-    )
+    loading_saving.write_csv(xy_train_val, "xy_regressor_fit", directory=regressor_directory)
+    loading_saving.write_csv(x_train_val, "x_regressor_fit", directory=regressor_directory)
+    loading_saving.write_csv(y_train_val, "y_regressor_fit", directory=regressor_directory)
+    loading_saving.write_csv(y_pred, "pred_regressor_fit", directory=regressor_directory)
 
-
-
+    # log model infos
+    model_infos = pd.DataFrame([best_model_params])
+    model_infos.loc[0, "best_model_name"] = best_model_name
+    model_infos.loc[0, "best_model_val_score"] = best_model_val_score
+    loading_saving.write_csv(model_infos, "model_infos", directory=regressor_directory)
 
     print(f"{__name__} executed")
+
 
 if __name__ == "__main__":
     config = ExtrapolationExperimentConfig()

14143bce871c356506beca8c5be6aa189712c127
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-04-09 18:28:48 +0200

    add doc string saying required packages on VM

--- setup.py
+++ setup.py
@@ -14,6 +14,24 @@ matplotlib      == 2.2.2,
 pandas        == 0.25.3, 
 networkx        ==   1.11"""
 
+'''
+installations done when setting up on VM python 3.11
+pandas 2.2.1
+scikit-learn 1.4.1
+hyperopt 0.2.7
+optuna 3.6.1
+matplotlib 3.8.4
+pyod 1.1.3
+wandb 0.16.6
+pydantic 2.6.4
+onnx 1.16.0
+skl2onnx 1.16.0
+seaborn 0.13.2
+plotly 5.20.0
+
+
+'''
+
 pgk_list = pkg_lite.replace(" ", "").split(",\n")
 
 setup(

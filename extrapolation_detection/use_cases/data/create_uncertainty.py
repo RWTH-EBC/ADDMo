ce83d4b54c1225d8e6dad25b204cc38696b2e573
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-04-10 16:46:21 +0200

    create uncertainty

--- extrapolation_detection/use_cases/data/create_uncertainty.py
+++ extrapolation_detection/use_cases/data/create_uncertainty.py
@@ -1,20 +1,34 @@
 import numpy as np
 import pandas as pd
 
+import os
+
+from extrapolation_detection.util import loading_saving
+
+simulation_data_name = "Carnot_mid"
+
+xy_tot: pd.DataFrame = loading_saving.read_csv(simulation_data_name, directory=None, index_col=False)
+
+# add uncertainty to last feature (normally the target)
+# uncertainty with a normal distribution with mean 0 and std dev 1
 # Set the seed for reproducibility
 np.random.seed(42)
-
 # Number of data points to generate
-num_data_points = 8787-4
-
+num_data_points = xy_tot.shape[0]
 # Generate random data from a normal distribution with mean 0 and std dev 1
-data = np.random.normal(loc=0, scale=1, size=num_data_points)
+std_1 = np.random.normal(loc=0, scale=1, size=num_data_points)
 
-# Create a pandas DataFrame with the generated data
-df = pd.DataFrame({'Data': data})
+desired_mean = 0
+desired_std = 0.02
+std_custom = desired_mean + (std_1 * desired_std)
 
-# Save the DataFrame to an Excel file
-file_name = 'gaussian_uncertainty.csv'
-df.to_csv(file_name, index=False)
+# Add the uncertainty to the last feature
+xy_tot.iloc[:, -1] = xy_tot.iloc[:, -1] + std_custom
 
-print(f"Data saved to '{file_name}'.")
+new_simulation_data_name = f"{simulation_data_name}_noise_m{desired_mean}_std{desired_std}"
+path = os.path.join("edited", new_simulation_data_name + ".csv")
+
+
+# save to folder
+xy_tot.to_csv(path, sep=";", index=False, header=True, encoding="utf-8")
+

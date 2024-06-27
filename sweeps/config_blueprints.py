82797f37981fd43436202d4af38ac1a4679699f5
mre-rpa <rishika.panwar@eonerc.rwth-aachen.de>
2024-06-27 10:23:21 +0200

    load sweeps added

--- sweeps/config_blueprints.py
+++ sweeps/config_blueprints.py
@@ -7,22 +7,22 @@ from core.s3_model_tuning.config.model_tuning_config import ModelTunerConfig
 from extrapolation_detection.use_cases.config.ed_experiment_config import ExtrapolationExperimentConfig
 
 
-def no_tuning_config(config: ExtrapolationExperimentConfig) -> ModelTunerConfig:
+def no_tuning_config(config: ExtrapolationExperimentConfig) :
     config.config_explo_quant.explo_grid_points_per_axis = 10
 
     config.config_model_tuning.models = ["SciKerasSequential"]
     config.config_model_tuning.hyperparameter_tuning_type = "NoTuningTuner"
     config.config_model_tuning.hyperparameter_tuning_kwargs = {
         "hyperparameter_set": {
-            "hidden_layer_sizes": [32],
+            "hidden_layer_sizes": [10],
             "loss": MeanSquaredError(),
-            "epochs": 5
+            "epochs": 50
         }
     }
     config.config_model_tuning.validation_score_metric = "neg_root_mean_squared_error"
     config.config_model_tuning.validation_score_mechanism = "cv"
     config.config_detector.detectors = ["KNN", "GP", "OCSVM"]
-    return config.config_model_tuning
+    return config
 
 
 def linear_regression_config(config: ExtrapolationExperimentConfig):
@@ -35,6 +35,6 @@ def linear_regression_config(config: ExtrapolationExperimentConfig):
 def tuning_config(config: ExtrapolationExperimentConfig):
     config = no_tuning_config(config)
     config.config_model_tuning.hyperparameter_tuning_type = "OptunaTuner"
-    config.config_model_tuning.hyperparameter_tuning_kwargs = {"n_trials": 200}
+    config.config_model_tuning.hyperparameter_tuning_kwargs = {"n_trials": 20}
     config.config_model_tuning.validation_score_metric = "neg_root_mean_squared_error"
     return config

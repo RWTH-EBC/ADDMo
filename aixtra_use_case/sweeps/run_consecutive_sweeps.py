c972ef931237eb43f7a5fcc3d2d8d42c0450a6eb
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-07-02 15:31:15 +0200

    big sweeping

--- aixtra_use_case/sweeps/run_consecutive_sweeps.py
+++ aixtra_use_case/sweeps/run_consecutive_sweeps.py
@@ -1,11 +1,25 @@
+from aixtra_use_case.sweeps import sweep_ed_usecase_boptest
 sweep_ed_usecase_boptest
 
-# from sweeps import sweep_ed_usecase_boptest_temp
-# sweep_ed_usecase_boptest_temp
+from aixtra_use_case.sweeps import sweep_ed_usecase_boptest_tuning
+sweep_ed_usecase_boptest_tuning
+
+from aixtra_use_case.sweeps import sweep_ed_usecase_boptest_noTune_scikit
+sweep_ed_usecase_boptest_noTune_scikit
+
+from aixtra_use_case.sweeps import sweep_ed_usecase_boptest_tuning_scikit
+sweep_ed_usecase_boptest_tuning_scikit
 
 from aixtra_use_case.sweeps import sweep_ed_usecase_carnot
-
 sweep_ed_usecase_carnot
 
-# from sweeps import sweep_ed_usecase_carnot_temp
-# sweep_ed_usecase_carnot_temp
+from aixtra_use_case.sweeps import sweep_ed_usecase_carnot_tuning
+sweep_ed_usecase_carnot_tuning
+
+from aixtra_use_case.sweeps import sweep_ed_usecase_carnot_tuning_scikit
+sweep_ed_usecase_carnot_tuning_scikit
+
+from aixtra_use_case.sweeps import sweep_ed_usecase_carnot_noTune_scikit
+sweep_ed_usecase_carnot_noTune_scikit
+
+
---
+++ aixtra_use_case/sweeps/sweep_ed_usecase_boptest_tuning_scikit.py
@@ -0,0 +1,104 @@
+import os
+
+import wandb
+
+from addmo.util.experiment_logger import ExperimentLogger
+from addmo.util.experiment_logger import LocalLogger
+from addmo.util.experiment_logger import WandbLogger
+from addmo.util.definitions import create_or_clean_directory
+
+from aixtra_use_case.sweeps import sweep_configs, config_blueprints
+
+from aixtra_use_case.config.ed_experiment_config import (
+    ExtrapolationExperimentConfig,
+)
+from aixtra_use_case import s2_tune_ml_regressor, s4_true_validity_domain, \
+    s8_1_coverage_convex_hull, s3_regressor_error_calculation, s8_3_coverage_tuned_ND, \
+    s5_tune_detector, s1_split_data, s8_4_coverage_true_validity, s6_detector_score_calculation, \
+    s8_2_coverage_grid_occupancy, s8_0_generate_grid
+
+
+def define_config():
+    # configure config
+    config = ExtrapolationExperimentConfig()
+    config.simulation_data_name = "Boptest_TAir_mid_ODE_noise_m0_std0.01"
+    config.experiment_name = "Empty"
+    config.name_of_target = "delta_reaTZon_y"
+    config.train_val_test_period = (0, 1488)
+    config.shuffle = False
+    config.grid_points_per_axis = 10
+    config.system_simulation = "BopTest_TAir_ODE"
+    config.true_outlier_threshold = 0.1
+
+    config.config_explo_quant.exploration_bounds = {
+        "TDryBul": (263.15, 303.15),
+        "HDirNor": (0, 1000),
+        "oveHeaPumY_u": (0, 1),
+        "reaTZon_y": (290.15, 300.15),
+        "delta_reaTZon_y": (-0.5, 0.5),
+    }
+
+    config = config_function(config)
+
+    config.config_model_tuning.models = ["ScikitMLP_TargetTransformed"]
+
+    return config
+
+
+def run_all():
+    # Define default values
+    config = define_config()
+
+    # Start wandb and overwrite default with sweep values where applicable
+    run = wandb.init(config=config.dict())
+
+    # update config with the experiment name of wandb run
+    wandb.config.update(
+        {"experiment_name": f"{project_name}_{run.name}"},
+        allow_val_change=True,
+    )
+
+    # convert config dict back to pydantic object
+    config = ExtrapolationExperimentConfig(**wandb.config)
+
+    create_or_clean_directory(config.experiment_folder)
+    LocalLogger.directory = os.path.join(config.experiment_folder, "local_logger")
+    WandbLogger.directory = config.experiment_folder
+
+    # locally log the config as well
+    LocalLogger.active = True
+    WandbLogger.active = False
+    ExperimentLogger.start_experiment(config=config)  # log config
+
+    # activate wandb logging again
+    WandbLogger.active = True
+
+    # Run scripts
+    s1_split_data.exe(config)
+    s2_tune_ml_regressor.exe(config)
+    s3_regressor_error_calculation.exe(config)
+    s4_true_validity_domain.exe(config)
+    s5_tune_detector.exe(config)
+    s6_detector_score_calculation.exe(config)
+    s8_0_generate_grid.exe(config)
+    s8_1_coverage_convex_hull.exe(config)
+    s8_2_coverage_grid_occupancy.exe(config)
+    s8_3_coverage_tuned_ND.exe(config)
+    s8_4_coverage_true_validity.exe(config)
+
+    ExperimentLogger.finish_experiment()
+
+
+####################################################################################################
+config_function = config_blueprints.tuning_config() # Todo: Set
+
+config_temp = define_config()
+
+project_name = f"3_{config_temp.simulation_data_name}" #Todo: Set
+# project_name = "Test"
+
+# sweep
+sweep_configuration = sweep_configs.sweep_several_tunings() #Todo: Set
+
+sweep_id = wandb.sweep(sweep_configuration, project=project_name)
+wandb.agent(sweep_id, function=run_all, project=project_name)

f44f475577d0aa088eda38d59ea0d6d3d4a52768
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-07-17 23:00:13 +0200

    more patient early stopping

--- addmo/s3_model_tuning/models/keras_models.py
+++ addmo/s3_model_tuning/models/keras_models.py
@@ -178,9 +178,9 @@ class SciKerasSequential(BaseKerasModel):
         hyperparameters['batch_size'] = 200
         hyperparameters['epochs'] = 1000
         hyperparameters['callbacks'] = [EarlyStopping(monitor='loss',
-                                                      min_delta=0.0001,
+                                                      min_delta=0.00001,
                                                       verbose=1,
-                                                      patience=10)]
+                                                      patience=50)]
 
 
         return hyperparameters

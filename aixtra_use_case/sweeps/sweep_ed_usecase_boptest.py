e6b3523746ad3ae17f3b13b9f7e2e509503cc63a
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-07-23 19:50:57 +0200

    make sweep paralleizable through conda and pycharm

--- aixtra_use_case/sweeps/sweep_ed_usecase_boptest.py
+++ aixtra_use_case/sweeps/sweep_ed_usecase_boptest.py
@@ -1,48 +1,38 @@
 import os
-
 import wandb
-
-from addmo.util.experiment_logger import ExperimentLogger
-from addmo.util.experiment_logger import LocalLogger
-from addmo.util.experiment_logger import WandbLogger
-from addmo.util.definitions import create_or_clean_directory
-
+from addmo.util.experiment_logger import ExperimentLogger, LocalLogger, WandbLogger
+from addmo.util.definitions import create_or_clean_directory, root_dir
 from aixtra_use_case.sweeps import sweep_configs, config_blueprints, config_blueprints_systems
-
-from aixtra_use_case.config.ed_experiment_config import (
-    ExtrapolationExperimentConfig,
+from aixtra_use_case.config.ed_experiment_config import ExtrapolationExperimentConfig
+from aixtra_use_case import (
+    s1_split_data, s2_tune_ml_regressor, s3_regressor_error_calculation,
+    s4_true_validity_domain, s5_tune_detector, s6_detector_score_calculation,
+    s8_0_generate_grid, s8_1_coverage_convex_hull, s8_2_coverage_grid_occupancy,
+    s8_3_coverage_tuned_ND, s8_4_coverage_true_validity
 )
-from aixtra_use_case import s2_tune_ml_regressor, s4_true_validity_domain, \
-    s8_1_coverage_convex_hull, s3_regressor_error_calculation, s8_3_coverage_tuned_ND, \
-    s5_tune_detector, s1_split_data, s8_4_coverage_true_validity, s6_detector_score_calculation, \
-    s8_2_coverage_grid_occupancy, s8_0_generate_grid, s9_data_coverage_grid, s9_data_coverage
 
-def run_all(config, project_name):
-    # Start wandb and overwrite default with sweep values where applicable
-    run = wandb.init(config=config.dict())
 
-    # update config with the experiment name of wandb run
-    wandb.config.update(
-        {"experiment_name": f"{project_name}_{run.name}"},
-        allow_val_change=True,
-    )
 
-    if config.config_model_tuning.models == ["ScikitLinearRegNoScaler"]:
-        wandb.run.name = f"LinReg_{run.name}"
-
-    # convert config dict back to pydantic object
+def run_all():
+    # Start wandb and get the config
+    run = wandb.init()
     config = ExtrapolationExperimentConfig(**wandb.config)
+
+    # Set up experiment name and folders
+    if config.config_model_tuning.models == ["ScikitLinearRegNoScaler"]:
+        wandb.run.name = f"LinReg_{wandb.run.name}"
+
+    # update config
+    config.experiment_name = f"{config.experiment_name}_{wandb.run.name}"
 
     create_or_clean_directory(config.experiment_folder)
     LocalLogger.directory = os.path.join(config.experiment_folder, "local_logger")
     WandbLogger.directory = os.path.join(config.experiment_folder, "wandb_logger")
 
-    # locally log the config as well
+    # Set up logging
     LocalLogger.active = True
     WandbLogger.active = False
-    ExperimentLogger.start_experiment(config=config)  # log config
-
-    # activate wandb logging again
+    ExperimentLogger.start_experiment(config=config)
     WandbLogger.active = True
 
     # Run scripts
@@ -57,29 +47,57 @@ def run_all(config, project_name):
     # s8_2_coverage_grid_occupancy.exe(config)
     s8_3_coverage_tuned_ND.exe(config)
     s8_4_coverage_true_validity.exe(config)
-    # s9_data_coverage.exe(config)
-    # s9_data_coverage_grid.exe(config)
 
     ExperimentLogger.finish_experiment()
 
 
-####################################################################################################
+def create_sweep():
+    # Set up the base configuration
+    config = ExtrapolationExperimentConfig()
+    config = config_blueprints_systems.config_ODEel_steady(config)
+    config = config_blueprints.linear_regression_config(config)
+    config.config_detector.detectors = ["KNN_untuned"]
 
-config = ExtrapolationExperimentConfig()
+    # Set up the project name
+    project_name = f"TEST_{config.simulation_data_name}"
 
-config = config_blueprints_systems.config_ODEel_steady(config)
-config = config_blueprints.linear_regression_config(config)
+    config.experiment_name = project_name
 
-config.config_detector.detectors = ["KNN_untuned"]
-
-project_name = f"TEST_{config.simulation_data_name}" #Todo: Set
-# project_name = "Test"
-
-# sweep
-sweep_configuration = sweep_configs.sweep_repetitions_only() #Todo: Set
-
-sweep_id = wandb.sweep(sweep_configuration, project=project_name)
+    # Set up the sweep configuration
+    sweep_configuration = sweep_configs.sweep_repetitions_only()
 
 
-if __name__ == '__main__':
-    wandb.agent(sweep_id, function=lambda: run_all(config, project_name), project=project_name)
+    # Add the config parameters to the sweep configuration
+    for key, value in config.dict().items():
+        if key not in sweep_configuration['parameters']:
+            sweep_configuration['parameters'][key] = {'value': value}
+
+    # Initialize the sweep
+    sweep_id = wandb.sweep(sweep_configuration, project=project_name)
+
+
+    print(f"Sweep created with ID: {sweep_id}")
+    print(f"Project name: {project_name}")
+    print(f"To run an agent in your pycharm console, use the following Python code:")
+    print(f"from aixtra_use_case.sweeps.sweep_ed_usecase_boptest import run_all")
+    print(f"import wandb")
+    print(f"wandb.agent('{sweep_id}', function=run_all, project='{project_name}')")
+
+    print("\nTo run an agent from the Anaconda command prompt, follow these steps:")
+    print("   conda activate ADDMo-Extra")
+    print(f"   cd /d {root_dir()}")
+    print(f"   python -m aixtra_use_case.sweeps.sweep_ed_usecase_boptest {sweep_id} {project_name}")
+
+    return sweep_id
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        # If a sweep ID is provided, run as an agent
+        wandb.agent(sys.argv[1], function=run_all, project=sys.argv[2])
+    else:
+        # If no sweep ID is provided, create a new sweep
+        sweep_id = create_sweep()
+        # wandb.agent(sweep_id, function=run_all)

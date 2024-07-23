d091a64e939db1e599fd8862327262f29888598c
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-07-23 23:17:22 +0200

    period to sep dez

--- aixtra_use_case/sweeps/config_blueprints_systems.py
+++ aixtra_use_case/sweeps/config_blueprints_systems.py
@@ -1,8 +1,11 @@
-def config_ODEel_steady(config):
+from aixtra_use_case.config.ed_experiment_config import ExtrapolationExperimentConfig
+
+def config_ODEel_steady(config: ExtrapolationExperimentConfig):
     config.simulation_data_name = "ODEel_steady"
     config.experiment_name = "Empty"
     config.name_of_target = "delta_reaTZon_y"
-    config.train_val_test_period = (29185, 35040) #November und Dezember
+    # config.train_val_test_period = (29185, 35040) #November und Dezember
+    config.train_val_test_period = (23329, 26208, 32065, 35040) #September und Dezember
     config.shuffle = False
     config.grid_points_per_axis = 10
     config.system_simulation = "BopTest_TAir_ODEel"
@@ -17,7 +20,7 @@ def config_ODEel_steady(config):
     }
     return config
 
-def config_carnot(config):
+def config_carnot(config: ExtrapolationExperimentConfig):
     config.simulation_data_name = "Carnot_mid_noise_m0_std0.02"
     config.experiment_name = "Empty"
     config.name_of_target = "$\dot{Q}_{heiz}$ in kW"

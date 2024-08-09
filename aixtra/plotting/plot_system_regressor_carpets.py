8d96001adc0c6b265fd52d3d5c978f6b6a12e22f
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-08-09 15:23:59 +0200

    improve carpet plotter

--- aixtra/plotting/plot_system_regressor_carpets.py
+++ aixtra/plotting/plot_system_regressor_carpets.py
@@ -15,7 +15,7 @@ path_to_regressor = r"R:\_Dissertationen\mre\Diss\08_Data_Plots_Analysis\1_MPC_B
 regressor = ModelFactory.load_model(path_to_regressor)
 
 def prediction(t_amb, rad_dir, u_hp, t_room):
-    return sys_sim.boptest_delta_T_air_physical_approximation_elcontrol(t_amb, rad_dir, u_hp, t_room)
+    return sys_sim.bestest900_ODE_VL(t_amb, rad_dir, u_hp, t_room)
 
 # def regressor_prediction(t_amb, rad_dir, u_hp, t_room):
 #     # Find the shape of the array inputs
@@ -39,7 +39,7 @@ def prediction(t_amb, rad_dir, u_hp, t_room):
 #     return prediction.reshape(shape)
 
 def regressor_prediction(t_amb, rad_dir, u_hp, t_room):
-    return sys_sim.bestest900_ODE_VL(t_amb, rad_dir, u_hp, t_room)
+    return sys_sim.bestest900_ODE_VL_COPcorr(t_amb, rad_dir, u_hp, t_room)
 
 # Define the bounds
 bounds = {
@@ -57,12 +57,12 @@ t_room = np.linspace(bounds["t_room"][0], bounds["t_room"][1], 50)
 
 # Create all possible combinations of variables
 combinations = [
-    (t_amb, rad_dir, "t_amb", "rad_dir"),
-    (t_amb, u_hp, "t_amb", "u_hp"),
+    (u_hp, t_amb, "u_hp", "t_amb"),
+    (u_hp, t_room, "u_hp", "t_room"),
+    (u_hp, rad_dir, "u_hp", "rad_dir"),
     (t_amb, t_room, "t_amb", "t_room"),
-    (rad_dir, u_hp, "rad_dir", "u_hp"),
+    (t_amb, rad_dir, "t_amb", "rad_dir"),
     (rad_dir, t_room, "rad_dir", "t_room"),
-    (u_hp, t_room, "u_hp", "t_room")
 ]
 
 
@@ -73,7 +73,7 @@ n_vars = len(variables)
 min_dict = {"t_room": np.min(t_room), "t_amb": np.min(t_amb), "rad_dir": np.min(rad_dir), "u_hp": np.min(u_hp)}
 max_dict = {"t_room": np.max(t_room), "t_amb": np.max(t_amb), "rad_dir": np.max(rad_dir), "u_hp": np.max(u_hp)}
 mean_dict = {"t_room": np.mean(t_room), "t_amb": np.mean(t_amb), "rad_dir": np.mean(rad_dir), "u_hp": np.mean(u_hp)}
-winter_dict = {"t_room": 294.15, "t_amb": 273.15, "rad_dir": 0, "u_hp": 1}
+winter_dict = {"t_room": 294.15, "t_amb": 273.15, "rad_dir": 0, "u_hp":0.5}
 
 used_dict = winter_dict
 
@@ -86,24 +86,27 @@ for i, (x, y, x_label, y_label) in enumerate(combinations, 1):
     ax = fig.add_subplot(2, 3, i, projection='3d')
     X, Y = np.meshgrid(x, y)
 
-    if x_label == "t_amb" and y_label == "rad_dir":
+    if x_label == "u_hp" and y_label == "t_amb":
+        Z1 = prediction(Y, used_dict["rad_dir"], X, used_dict["t_room"])
+        Z2 = regressor_prediction(Y, used_dict["rad_dir"], X, used_dict["t_room"])
+    elif x_label == "u_hp" and y_label == "rad_dir":
+        Z1 = prediction(used_dict["t_amb"], Y, X, used_dict["t_room"])
+        Z2 = regressor_prediction(used_dict["t_amb"], Y, X, used_dict["t_room"])
+    elif x_label == "u_hp" and y_label == "t_room":
+        Z1 = prediction(used_dict["t_amb"], used_dict["rad_dir"], X, Y)
+        Z2 = regressor_prediction(used_dict["t_amb"], used_dict["rad_dir"], X, Y)
+    elif x_label == "t_amb" and y_label == "rad_dir":
         Z1 = prediction(X, Y, used_dict["u_hp"], used_dict["t_room"])
         Z2 = regressor_prediction(X, Y, used_dict["u_hp"], used_dict["t_room"])
-    elif x_label == "t_amb" and y_label == "u_hp":
-        Z1 = prediction(X, used_dict["rad_dir"], Y, used_dict["t_room"])
-        Z2 = regressor_prediction(X, used_dict["rad_dir"], Y, used_dict["t_room"])
     elif x_label == "t_amb" and y_label == "t_room":
         Z1 = prediction(X, used_dict["rad_dir"], used_dict["u_hp"], Y)
         Z2 = regressor_prediction(X, used_dict["rad_dir"], used_dict["u_hp"], Y)
-    elif x_label == "rad_dir" and y_label == "u_hp":
-        Z1 = prediction(used_dict["t_amb"], X, Y, used_dict["t_room"])
-        Z2 = regressor_prediction(used_dict["t_amb"], X, Y, used_dict["t_room"])
     elif x_label == "rad_dir" and y_label == "t_room":
         Z1 = prediction(used_dict["t_amb"], X, used_dict["u_hp"], Y)
         Z2 = regressor_prediction(used_dict["t_amb"], X, used_dict["u_hp"], Y)
-    else:  # u_hp and t_room
-        Z1 = prediction(used_dict["t_amb"], used_dict["rad_dir"], X, Y)
-        Z2 = regressor_prediction(used_dict["t_amb"], used_dict["rad_dir"], X, Y)
+    else:
+        print(x_label, y_label)
+
 
     surf1 = ax.plot_surface(X, Y, Z1, cmap='viridis', alpha=0.7)
     surf2 = ax.plot_surface(X, Y, Z2, cmap='plasma', alpha=0.7)

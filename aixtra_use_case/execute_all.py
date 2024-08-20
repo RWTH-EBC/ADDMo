bbf4c061c1a7fe40f73b755b77d9c30021244ba3
Martin Rätz <mraetz@eonerc.rwth-aachen.de>
2024-08-20 15:10:06 +0200

    execute all

--- aixtra_use_case/execute_all.py
+++ aixtra_use_case/execute_all.py
@@ -33,11 +33,8 @@ from aixtra_use_case import (
 # configure config
 from aixtra_use_case.sweeps import config_blueprints, config_blueprints_systems
 config = ExtrapolationExperimentConfig()
-config = config_blueprints_systems.config_bes_steady(config)
-config = config_blueprints.no_tuning_config(config)
-
-# config.train_val_test_period = (23329, 26208, 32065, 35040) #September und Dezember
-
+config = config_blueprints_systems.config_bes_VLCOPcorr_steady_NovDezSelect(config)
+config = config_blueprints.linear_regression_config(config)
 
 # Configure the logger
 create_or_clean_directory(config.experiment_folder)
@@ -55,12 +52,12 @@ s2_tune_ml_regressor.exe(config)
 s3_regressor_error_calculation.exe(config)
 s4_true_validity_domain.exe(config)
 s4_gradient.exe(config)
-# s5_tune_detector.exe(config)
-# s6_detector_score_calculation.exe(config)
+s5_tune_detector.exe(config)
+s6_detector_score_calculation.exe(config)
 s8_0_generate_grid.exe(config)
-# # # s8_1_coverage_convex_hull.exe(config)
-# # # s8_2_coverage_grid_occupancy.exe(config)
-# # s8_3_coverage_tuned_ND.exe(config)
+# s8_1_coverage_convex_hull.exe(config)
+# s8_2_coverage_grid_occupancy.exe(config)
+s8_3_coverage_tuned_ND.exe(config)
 s8_4_coverage_true_validity.exe(config)
 s8_5_coverage_gradient.exe(config)
 # s9_data_coverage.exe(config)

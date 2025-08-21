31cfad49fbdc899bea50c83778c992ab0ec3bbde
Rishika Panwar <rishika.panwar@rwth-aachen.de>
2025-08-22 01:53:12 +0200

    gh cleaned

--- addmo/s3_model_tuning/scoring/metrics/custom_metrics.py
+++
@@ -1,1 +0,0 @@
-"""A possible metric is e.g. AIC. Adhere to the AbstractMetric class"""
--- addmo/util/data_handling.py
+++
@@ -1,4 +0,0 @@
-def split_target_features(target_name, xy):
-    x = xy.drop(target_name, axis=1)
-    y = xy[target_name]
-    return (x, y)
